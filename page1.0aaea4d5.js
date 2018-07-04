a5aca4d6982c3b8b5186042dd14a487d1f3d443b
Diogo Cezar <diogo@diogocezar.com>
2018-07-04 09:49:33 -0300

    Adjustments

--- page1.0aaea4d5.js
+++ page1.0aaea4d5.js
@@ -1,6 +1,3 @@
 parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRequire,o="function"==typeof require&&require;function u(n,t){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!t&&f)return f(n,!0);if(i)return i(n,!0);if(o&&"string"==typeof n)return o(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,p,l,l.exports,this)}return r[n].exports;function p(e){return u(p.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=i,u.register=function(r,n){e[r]=[function(e,r){r.exports=n},{}]};for(var f=0;f<n.length;f++)u(n[f]);if(n.length){var c=u(n[n.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=c:"function"==typeof define&&define.amd?define(function(){return c}):t&&(this[t]=c)}return u}({36:[function(require,module,exports) {
-
-},{}],31:[function(require,module,exports) {
-"use strict";require("../../assets/scss/pages/page2.scss"),console.log("page2");
-},{"../../assets/scss/pages/page2.scss":36}]},{},[31], null)
-//# sourceMappingURL=/page2.c160fb3d.map
+function n(n,a){if(!(n instanceof a))throw new TypeError("Cannot call a class as a function")}var a=function a(){n(this,a),console.log("Start Page1")};new a;
+},{}]},{},[36], null)
